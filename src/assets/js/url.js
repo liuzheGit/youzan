14b742f9a38a43a60e17f941d4e19e618db0a35c
liuzhe <zhethink@gmail.com>
2018-11-29 16:10:52 +0800

    后台配置完成, 添加所有接口

--- src/assets/js/url.js
+++ src/assets/js/url.js
@@ -1,6 +1,28 @@
 let url = {
+  // 首页
   'bannerList': 'bannerList',
-  'goodsLists': 'goods/gitLists'
+  'goodsLists': 'goods/gitLists',
+  // 商品详情
+  'goodsDetail': 'goods/detail',
+  'goodsDeal': 'goods/deal',
+  'goodsEvaluation': 'goods/evaluation',
+  // 搜索
+  'searchList': 'search/list',
+  // 分类
+  'categoryTop': 'category/topList',
+  'categoryRank': 'category/rank',
+  'categorySub': 'category/subList',
+  // 购物车
+  'cartList': 'cart/list',
+  'cartAdd': 'cart/add',
+  'cartUpdate': 'cart/update',
+  // 个人地址管理
+  'addressList': 'address/list',
+  'addressAdd': 'address/add',
+  'addressRemove': 'address/remove',
+  'addressUpdate': 'address/update',
+  'addressDefault': 'address/setDefault'
+
 };
 
 let host = 'http://rap2api.taobao.org/app/mock/119078/';
